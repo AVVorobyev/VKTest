8eac340c2b7d970b37c206f54d4fbd7d8dc0c2fa
AlexanderVorobyev <passer.alexander@gmail.com>
2023-05-09 15:13:44 +0700

    add Guard class

---
+++ Core/Guard.cs
@@ -0,0 +1,15 @@
+﻿namespace Core
+{
+    public static class Guard
+    {
+        public static T NotNull<T>(T? value, string name) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+
+            return value;
+        }
+    }
+}
